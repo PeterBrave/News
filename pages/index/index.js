7bbb2a397aedf6920e4398b77a6a82330794632c
董士洋 <toushiyou@dongshiyangdeMacBook-Pro.local>
2018-07-31 20:45:45 +0800

    1-3实现了导航栏的样式，并加入了点击事件，可以动态的修改添加样式

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,15 @@
-Page({})
+Page({
+  data: {
+    _num: 0,
+  },
+  clickNum: function (e) {
+    //console.log(e.target.dataset.num)
+    this.setData({
+      _num: e.target.dataset.num
+    })
+  },
+  onLoad: function (options) {
+
+  }
+
+})
