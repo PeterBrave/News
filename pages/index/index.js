0553c76d2c919f9f10df5f5c660f0710c1613e9d
董士洋 <toushiyou@dongshiyangdeMacBook-Pro.local>
2018-08-01 23:33:22 +0800

    1-4完成了新闻小程序的列表页面设计，以及API的使用。此处仅限于gn这一栏。了解了日期Date的转换。

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,7 @@
 Page({
   data: {
     _num: 0,
+    newsList: []
   },
   clickNum: function (e) {
     //console.log(e.target.dataset.num)
@@ -8,8 +9,28 @@ Page({
       _num: e.target.dataset.num
     })
   },
-  onLoad: function (options) {
-
+  onLoad: function () {
+      wx.request({
+        url: 'https://test-miniprogram.com/api/news/list',
+        data: {
+          type: 'gn'
+        },
+        success: res => {
+          //console.log(res)
+          let result = res.data.result
+          for (let i=0; i<result.length; i++) {
+            let b = new Date(result[i].date)
+            let hour = b.getUTCHours().toString().padStart(2, '0')
+            let minutes = b.getUTCMinutes().toString().padStart(2, '0')
+            result[i].date = `${hour}:${minutes}`
+          }
+          //console.log(result)
+          this.setData({
+            newsList: result
+          })
+        }
+      })
+      // console.log(this.data.newsList[0].date.substring(11,16))
   }
 
 })
