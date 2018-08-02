1b3035b111cdd7da34eafa3d762cecaa28fe8c41
董士洋 <toushiyou@dongshiyangdeMacBook-Pro.local>
2018-08-02 11:35:59 +0800

    1-5实现了新闻小程序在各个新闻类别之间的切换。修复了hot-card背景图片size一直变化的bug

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,36 +1,60 @@
 Page({
   data: {
     _num: 0,
-    newsList: []
+    newsList: [],
+    newsTag: 'gn'
   },
   clickNum: function (e) {
     //console.log(e.target.dataset.num)
+    let num = e.target.dataset.num
+    let tag = ''
+    switch(num) {
+      case '1': tag = 'gn';
+      break;
+      case '2': tag = 'gj';
+      break;
+      case '3': tag = 'cj';
+      break;
+      case '4': tag = 'yl';
+      break;
+      case '5': tag = 'js';
+      break;
+      case '6': tag = 'ty';
+      break;
+      case '7': tag = 'other';
+      break;
+    }
     this.setData({
-      _num: e.target.dataset.num
+      _num: num,
+      newsTag: tag
     })
+    this.getNews()
   },
   onLoad: function () {
-      wx.request({
-        url: 'https://test-miniprogram.com/api/news/list',
-        data: {
-          type: 'gn'
-        },
-        success: res => {
-          //console.log(res)
-          let result = res.data.result
-          for (let i=0; i<result.length; i++) {
-            let b = new Date(result[i].date)
-            let hour = b.getUTCHours().toString().padStart(2, '0')
-            let minutes = b.getUTCMinutes().toString().padStart(2, '0')
-            result[i].date = `${hour}:${minutes}`
-          }
-          //console.log(result)
-          this.setData({
-            newsList: result
-          })
+      this.getNews()
+  },
+  getNews() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data: {
+        type: this.data.newsTag
+      },
+      success: res => {
+        //console.log(res)
+        let result = res.data.result
+        for (let i = 0; i < result.length; i++) {
+          let b = new Date(result[i].date)
+          let hour = b.getUTCHours().toString().padStart(2, '0')
+          let minutes = b.getUTCMinutes().toString().padStart(2, '0')
+          result[i].date = `${hour}:${minutes}`
         }
-      })
-      // console.log(this.data.newsList[0].date.substring(11,16))
+        //console.log(result)
+        this.setData({
+          newsList: result
+        })
+      }
+    })
   }
+
 
 })
